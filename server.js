a5bdd35c60ff2b323cc87fc627df6d8a68cb42a8
seanlanders92 <seanlanders92@gmail.com>
2020-02-26 10:26:20 -0800

    added schema.sql and dummy data for the database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const pg = require('pg');
 const superagent = require('superagent');
 require('ejs');
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
 
 app.get('/', renderHomePage);
 app.get('/searches/new', newSearch)
