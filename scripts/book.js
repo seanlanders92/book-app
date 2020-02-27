e1c4c7f160ef8baf2f7110ae2f7120b4d8c12865
brody-rebne <bsrebne@gmail.com>
2020-02-27 10:35:26 -0800

    fixed typo again

--- scripts/book.js
+++ scripts/book.js
@@ -2,7 +2,7 @@
 
 function Book(obj) {
   this.title = obj.volumeInfo.hasOwnProperty('title') ? obj.volumeInfo.title : 'No Title Available';
-  this.author = obj.volumeInfo.hasOwnProperty('author') ? obj.volumeInfo.authors[0] : 'No Author Available';
+  this.author = obj.volumeInfo.hasOwnProperty('authors') ? obj.volumeInfo.authors[0] : 'No Author Available';
   this.description = obj.volumeInfo.hasOwnProperty('description') ? obj.volumeInfo.description : 'No Description Available';
   let regex = /^https/;
   let thumbUrl = obj.volumeInfo.imageLinks.hasOwnProperty('thumbnail') ? obj.volumeInfo.imageLinks.thumbnail : 'https://images.pexels.com/photos/2340254/pexels-photo-2340254.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
